ad79e13e63c493b03b02acbafbd4d6395cf2f1a3
brunnomsants <brunnomurilo1@gmail.com>
2024-10-23 10:01:03 -0300

    code

---
+++ ps.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isValidSudoku(board):
+        row = [[] for _ in range(9)]
+        column = [[] for _ in range(9)]
+        group = [[] for _ in range(9)]
+        for i in range(9):
+            for j in range(9):
+                n = board[i][j]
+                if n!='.':
+                    index = (i // 3) * 3 + (j // 3)
+                    if n in column[j] or n in row[i] or n in group[index]:
+                        return False
+                    
+                    row[i].append(n)
+                    column[j].append(n)
+                    group[index].append(n)
+                    
+        return True
+        
+
+    def board():
+        board = [[] for i in range(9)]
+        for i in range(0, 9):
+            board[i] = input().split()
+        return print(Solution.isValidSudoku(board))
+
+Solution.board() 
+
+
+
